bd5b5fcdd48c08ffddaeef39733f60590464a730
listyaomii <220711679@students.uajy.ac.id>
2024-09-25 15:30:52 +0700

    Update Tugas_Modul 2_B_Listya_11679.py

--- Tugas_B_11679/Tugas_Modul 2_B_Listya_11679.py
+++ Tugas_B_11679/Tugas_Modul 2_B_Listya_11679.py
@@ -4,7 +4,7 @@ import pickle
 import os
 
 #Load model yang sudah dibuat dari Jupyter Notebook
-model = 'GBT_heartDisease_model.pkl'
+model_path = 'GBT_heartDisease_model.pkl'
 
 with open(model,'rb') as f:
     loaded_model = pickle.load(f)
